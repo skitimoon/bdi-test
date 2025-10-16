7091e2861b04c814fd9c23d043b6d23e1b47bef2
skitimoon <s.kitimoon@gmail.com>
2025-10-16 15:27:00 +0700

    Clean up unneccesary parts

--- devenv.nix
+++ devenv.nix
@@ -1,11 +1,4 @@
-{
-  pkgs,
-  lib,
-  config,
-  inputs,
-  ...
-}: {
-  # https://devenv.sh/packages/
+{pkgs, ...}: {
   packages = [pkgs.git];
 
   languages.python = {
@@ -16,34 +9,20 @@
     };
   };
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
+  scripts.testhello.exec = ''
     uv run pytest
   '';
 
-  # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
     git --version # Use packages
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
 
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
     git --version | grep --color=auto "${pkgs.git.version}"
   '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
 
   # See full reference at https://devenv.sh/reference/options/
 }
